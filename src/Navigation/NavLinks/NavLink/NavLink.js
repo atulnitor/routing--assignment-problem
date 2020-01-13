be11e2c9e75e4946ad3505bbb07e912f1ba4ebee
Atul Gaikwad <atul.gaikwad@nitorinfotech.net>
2020-01-13 11:16:58 +0530

    Added routing problem assignment

---
+++ src/Navigation/NavLinks/NavLink/NavLink.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './NavLink.css'
+
+const navLink = props => {
+    return (
+        <NavLink
+        to={ `/${props.path}` }
+        className = "NavLink"
+        activeStyle = {{
+            backgroundColor: '#44ce81'
+        }}>{ props.link }</NavLink>
+    )
+}
+
+export default navLink;
---
+++ src/containers/Users/Users.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+    render () {
+        return (
+            <div>
+                <h1>The Users Page</h1>
+                <h2> User Page Info </h2>
+            </div>
+        );
+    }
+}
+
+export default Users;
